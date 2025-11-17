361ba269a21b7b415d1b20ca46b3b65d9a7fea89
Debanjan Basu <debanjanbasu2006@gmail.com>
2025-11-17 20:19:25 +1100

    added in spn

--- iac/entra_oauth.tf
+++ iac/entra_oauth.tf
@@ -102,6 +102,12 @@ resource "azuread_application" "agentcore_app" {
   tags = local.entra_app_tags
 }
 
+# Service principal for the AgentCore Gateway application
+resource "azuread_service_principal" "agentcore_sp" {
+  client_id = azuread_application.agentcore_app.client_id
+  owners    = [data.azuread_client_config.current.object_id]
+}
+
 # Grant organization-wide admin consent for Microsoft Graph permissions using app role assignments
 # This pre-approves the required app roles so users don't see consent prompts
 # NOTE: Converting delegated permissions to app roles
@@ -110,7 +116,7 @@ resource "azuread_application" "agentcore_app" {
 # Skip this assignment if running with limited permissions
 resource "azuread_app_role_assignment" "graph_permissions" {
   app_role_id         = local.microsoft_graph_user_read_all_app_role_id
-  principal_object_id = azuread_application.agentcore_app.object_id
+  principal_object_id = azuread_service_principal.agentcore_sp.object_id
   resource_object_id  = data.azuread_service_principal.microsoft_graph.object_id
 }
 
