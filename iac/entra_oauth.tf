4988b36a87d49e5f793c97e2be9bdb694d65f0f2
Debanjan Basu <debanjanbasu2006@gmail.com>
2025-11-25 13:25:10 +1100

    added in timeout

--- iac/entra_oauth.tf
+++ iac/entra_oauth.tf
@@ -128,4 +128,11 @@ resource "azuread_application" "agentcore_app" {
       password,
     ]
   }
+
+  timeouts {
+    create = "15m"
+    read = "15m"
+    update = "15m"
+    delete = "15m"
+  }
 }
